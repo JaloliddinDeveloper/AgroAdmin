539c7efa8719ef272346933d368c0ed48a189a8c
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-01-27 13:20:01 +0500

    JadvalIkki WIP

---
+++ AgroAdmin/Models/ViewModels/TableTwoPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+using AgroAdmin.Models.Foundations.ProductTwos;
+
+namespace AgroAdmin.Models.ViewModels
+{
+    public class TableTwoPageViewModel
+    {
+        public int ProuctTwoId { get; set; }
+        public string ProductTwoName { get; set; }
+        public IEnumerable<TableTwo> TableTwoes { get; set; }
+    }
+}
